fbb85197b5978d60cc2139793fe8e7d4347298cc
ZumTheZazaKing <zahidi85543@gmail.com>
2024-06-23 22:58:23 +0800

    Basic setup

--- server/index.js
+++ server/index.js
@@ -1,1 +1,14 @@
+const express = require('express')
+const app = express()
+const http = require('http')
+const cors = require('cors')
 const fs = require('fs')
+
+const PORT = 3001
+
+app.use(cors())
+const server = http.createServer(app)
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
