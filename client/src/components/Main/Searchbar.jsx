f211942e499b3be5dc53917bd42121cbf9ac8a93
ZumTheZazaKing <zahidi85543@gmail.com>
2024-06-26 21:10:35 +0800

    Added clear search

--- client/src/components/Main/Searchbar.jsx
+++ client/src/components/Main/Searchbar.jsx
@@ -1,19 +1,29 @@
 import { useContext } from "react";
 import { Context } from "../../App";
+import { IoClose } from "react-icons/io5";
 
 const Searchbar = () => {
 
     const { searchQuery, setSearchQuery } = useContext(Context)
 
+    const clearSearch = () => {
+        setSearchQuery("")
+    }
+
     return (
-        <div className=" w-[90vw] sm:w-[70vw] mx-auto">
+        <div className="w-[90vw] sm:w-[70vw] mx-auto flex items-center bg-white px-4 p-2 rounded-[50px] shadow-lg">
             <input
-                className="w-full px-4 p-2 rounded-[50px] shadow-lg"
+                className="flex-[1] outline-none"
                 type="text"
                 value={searchQuery}
                 onChange={e => setSearchQuery(e.target.value)}
                 placeholder="Search food... (etc. Chicken, Fish)"
             />
+            {searchQuery.trim() !== "" &&
+                <button onClick={clearSearch}>
+                    <IoClose className="text-2xl" />
+                </button>
+            }
         </div>
     )
 }; export default Searchbar
