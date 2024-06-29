0e18bec330eac370e9aabfed0ba88fbc5eeade08
ZumTheZazaKing <zahidi85543@gmail.com>
2024-06-29 16:36:18 +0800

    Added logo

--- client/src/components/Main/Header.jsx
+++ client/src/components/Main/Header.jsx
@@ -1,6 +1,9 @@
+import Logo from '../../assets/logo.png'
+
 const Header = () => {
     return (
         <header className=" text-center px-4 py-2">
+            <img src={Logo} alt="zumcalories logo" width={75} className='mx-auto' />
             <h1 className="font-semibold text-2xl">ZumCalories</h1>
             <p>Count your calories!</p>
         </header>
